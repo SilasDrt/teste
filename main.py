cfb083cf402203e3665d6c91fcd704aa23b7ef14
Silas Duarte <silas.duarte15@gmail.com>
2025-09-14 19:31:22 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+#oq e o Yfinance
+import yfinance as yf
+import pandas as pd
+from datetime import datetime
+
+import streamlit as st
+#como usar?
+st.markdown('# Analisando empresas')
+
+st.text_input('Preencha o Ticker Code: ',key ='tickercode',value = 'GOOG')
+st.markdown(f'## Ultimas noticias da {st.session_state.tickercode} :')
+
+ticker = st.session_state.tickercode
+
+data = yf.Ticker(ticker)
+
+data_news=pd.DataFrame(data.news)
+content_expanded = data_news['content'].apply(pd.Series)
+
+data_news2 = content_expanded[['title','contentType','canonicalUrl','displayTime']]
+st.dataframe(data_news2)
+
+end_date = datetime.now().strftime('%Y-%m-%d')
+data_hist = data.history(period='max',start='2019-03-16',end =end_date,interval='5d' )
+data_hist = data_hist.reset_index()
+
+st.markdown('# Construa seu gráfico')
+
+ex = st.selectbox('Eixo x:',data_hist.columns)
+ey = st.selectbox('Eixo y:',data_hist.columns)
+st.markdown(f'## Gráfico {ex} x {ey}')
+st.line_chart(data_hist,x=f'{ex}',y=f'{ey}')
+
+print(data_news2)
